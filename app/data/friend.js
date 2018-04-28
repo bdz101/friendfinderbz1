fd687af808f8f1e3174000df2d1cca15bb0f2b56
Brian Zeuch <bz901610@ohio.edu>
2018-04-27 20:00:40 -0400

    test

---
+++ app/data/friend.js
@@ -0,0 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Donald Trump',
+    profilePic: "https://vignette.wikia.nocookie.net/walkingdead/images/5/54/Link.jpg/revision/latest?cb=20140613144939",
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
